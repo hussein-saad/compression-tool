ed32e98f7cdcdb2a2e85e99b72325e4e7093153d
hussein-saad <hussein.saad2021@gmail.com>
2024-06-07 23:11:26 +0300

    Add cors dependency and enable CORS in backend app

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const compressionRoutes = require('./routes/compressionRoutes');
 
 const app = express();
 
+
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/compression', compressionRoutes);
